d002126c83a663b2ff576ea78986ed5d68ba3ba7
catur sukoco <catur.sukoco@gmail.com>
2022-03-24 19:58:59 +0800

    list python code

---
+++ main_second_code.py
@@ -0,0 +1,6 @@
+"""
+This is Python code for making code based on flowchart
+"""
+print('Mom said "Please go to the shop and buy 1 bottle of milk, if they have eggs, bring 6"')
+print('Son said "Oke mom"')
+print('Son go to market')
